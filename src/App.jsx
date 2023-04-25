f5acf89eedac50ae65da4ef8c4db42ea92a3e44f
Anrsgrl <esgerlianar003@gmail.com>
2023-04-25 13:45:59 +0400

    Structure prepared

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import './App.scss';
+import { Route, Routes } from 'react-router-dom';
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Home from "./pages/Home/Home";
+import Shop from "./pages/Shop/Shop";
+import Cart from "./pages/Cart/Cart";
+import NotFound from "./pages/NotFound";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
