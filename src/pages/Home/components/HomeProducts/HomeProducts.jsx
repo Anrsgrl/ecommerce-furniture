68b46bfa9af51ca1b8d5938f515a50d17d714ecf
Anrsgrl <esgerlianar003@gmail.com>
2023-04-25 21:28:53 +0400

    product cards done/responsive

--- src/pages/Home/components/HomeProducts/HomeProducts.jsx
+++ src/pages/Home/components/HomeProducts/HomeProducts.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-import ProductCard from '../../../../components/ProductCard/ProductCard'
-
+import ProductCard from '../../../../components/ProductCard/ProductCard';
+import "./homeProducts.scss";
 const HomeProducts = () => {
     return (
-        <div className="home-products page-container">
+        <div className="home-products page-container text-center">
+            <h3>Our Latest Products</h3>
+            <div className="divider-part">
+                <div className="divider"></div>
+            </div>
             <div className="row">
                 <ProductCard />
             </div>
