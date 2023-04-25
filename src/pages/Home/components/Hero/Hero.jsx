dff68f69ea65faaafd24e439f5e1ba4db45df2da
Anrsgrl <esgerlianar003@gmail.com>
2023-04-25 16:20:47 +0400

    hero done/responsive

---
+++ src/pages/Home/components/Hero/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./hero.scss";
+const Hero = () => {
+    return (
+        <div className="hero page-container">
+            <div className="hero-content">
+                <h1>Style <br /> Comfort& <br /> Affordable</h1>
+                <button className='general-button mt-4'>Explore Store</button>
+            </div>
+        </div>
+    )
+}
+
+export default Hero;
