c005c9b3d5ac6b143d837a8391781a6e53451401
Anrsgrl <esgerlianar003@gmail.com>
2023-04-26 11:56:55 +0400

    Footer done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <footer style={{ background: "#3a3a3a" }} className='d-flex justify-content-between page-container p-5'>
+            <div className="footer-left">
+                <span className='text-white'>Copyright © 2023 Furniture Store</span>
+            </div>
+            <div className="footer-right">
+                <span className='pe-1 text-white'>Made by</span>
+                <span className='fw-bold' style={{ color: "#78909c", cursor: "pointer" }} onClick={() => { window.open("https://github.com/Anrsgrl") }}>Anrsgrl</span>
+            </div>
+        </footer>
     )
 }
 
-export default Footer
+export default Footer;
