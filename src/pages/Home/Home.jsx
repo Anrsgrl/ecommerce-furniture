d94d5fa339e900d01cc982ae44a7e9b01ee1e2fc
Anrsgrl <esgerlianar003@gmail.com>
2023-04-26 11:41:30 +0400

    HomeConnect done/responsive

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,6 +5,7 @@ import HomeSale from './components/HomeSale/HomeSale';
 import HomeServices from './components/HomeServices/HomeServices';
 import HomeProducts from './components/HomeProducts/HomeProducts';
 import Testimonials from './components/Testimonials/Testimonials';
+import HomeConnect from './components/HomeConnect/HomeConnect';
 
 const Home = () => {
     return (
@@ -15,6 +16,7 @@ const Home = () => {
             <HomeSale />
             <HomeProducts />
             <Testimonials />
+            <HomeConnect />
         </div>
     )
 }
